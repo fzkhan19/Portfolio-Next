b497818a52c98fea509b91297295e31e722a1cd3
Faiz Khan <faizpathan1717@gmail.com>
2024-09-05 17:16:07 +0530

    feat: site completed

--- src/lib/siteMetaData.ts
+++ src/lib/siteMetaData.ts
@@ -1,14 +1,15 @@
 // please update required information
 export const siteMetadata = {
-	title: "Blog | Faiz Khan",
-	author: "Faiz Khan",
-	headerTitle: "Blog | Faiz Khan",
-	description: "A blog created with Next.js, Tailwind.css and contentlayer.",
+	title: "Faiz Khan | Portfolio",
+	author: "Faiz Khan | Portfolio",
+	headerTitle: "Faiz Khan | Portfolio",
+	description:
+		"A portfolio created with Next.js, Tailwind.css and contentlayer.",
 	language: "en-us",
 	theme: "system", // system, dark or light
 	siteUrl: "https://faiz-khan.in", // your website URL
 	siteLogo: "/sakura.png",
-	socialBanner: "/social-banner.png", // add social banner in the public folder
+	socialBanner: "/opengraph-image.gif", // add social banner in the public folder
 	email: "faizpathan1717@gmail.com",
 	github: "https://github.com/fzkhan19",
 	twitter: "https://twitter.com/",
